957fa347958c4445763d3877f0bf52c31beb2f1a
halephu01 <halephu01@gmail.com>
2023-11-10 17:38:05 +0700

    complete sign in with google

--- app/src/main/java/com/example/tprom/Login.java
+++ app/src/main/java/com/example/tprom/Login.java
@@ -3,6 +3,7 @@ package com.example.tprom;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,34 +11,104 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
+
+/** @noinspection ALL*/
 public class Login extends AppCompatActivity {
     private EditText ed_username, ed_password;
     private FirebaseAuth mAuth;
+    private GoogleSignInClient signinClient;
+    private GoogleSignInOptions signInOptions;
+    int RC_SIGN_IN = 20;
+
 
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         mAuth= FirebaseAuth.getInstance();
 
         ed_username=findViewById(R.id.ed_username);
         ed_password=findViewById(R.id.ed_password);
 
         TextView tv_login = findViewById(R.id.tv_login);
-        
+        TextView tv_signGoogle = findViewById(R.id.tv_login_google);
         TextView tv_register = findViewById(R.id.tv_register);
 
         tv_login.setOnClickListener(v -> login());
 
         tv_register.setOnClickListener(v -> register());
+
+        /*ed_password.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    int drawableEnd = 2;
+                    if (event.getRawX() >= (ed_Password.getRight() - ed_Password.getCompoundDrawables()[drawableEnd].getBounds().width())) {
+
+                    }
+                }
+                return false;
+            }
+        });*/
+
+        //đăng nhập bằng google
+        signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        signinClient = GoogleSignIn.getClient(this,signInOptions);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+        if(acct!=null){
+            loginWithGoogle();
+        }
+
+        tv_signGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
     }
+    void signIn(){
+        Intent signInIntent = signinClient.getSignInIntent();
+        startActivityForResult(signInIntent,1000);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                task.getResult(ApiException.class);
+                loginWithGoogle();
+            } catch (ApiException e) {
+                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    void loginWithGoogle(){
+        finish();
+        Intent intent=new Intent(Login.this,MainActivity.class);
+        startActivity(intent);
+    }
+
 
     private void register(){
         Intent i = new Intent(Login.this,Register.class);
         startActivity(i);
     }
+
     private void login(){
         String username, password;
         username=ed_username.getText().toString();
@@ -64,7 +135,5 @@ public class Login extends AppCompatActivity {
             }
         });
     }
-
-
 
 }
--- app/src/main/java/com/example/tprom/MainActivity.java
+++ app/src/main/java/com/example/tprom/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.tprom;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
+    private GoogleSignInOptions signInOptions;
+    private GoogleSignInClient signInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btSignout = findViewById(R.id.bt_logout);
+
+        signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        signInClient = GoogleSignIn.getClient(this,signInOptions);
+
+        GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+
+        btSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+
+    }
+    void signOut(){
+        signInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                finish();
+                startActivity(new Intent(MainActivity.this,Login.class));
+            }
+        });
     }
 }
