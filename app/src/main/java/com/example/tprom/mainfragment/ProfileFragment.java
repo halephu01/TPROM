349f23b335c3a614d7e6d0abc4cab1b5d1e92bfd
Đạt Nguyễn Thành <21521939@ms.uit.edu.vn>
2024-01-01 17:01:05 +0700

    Commited

--- app/src/main/java/com/example/tprom/mainfragment/ProfileFragment.java
+++ app/src/main/java/com/example/tprom/mainfragment/ProfileFragment.java
@@ -1,17 +1,22 @@
 package com.example.tprom.mainfragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.tprom.ultis.AppConstants;
 import com.example.tprom.MainActivity;
 import com.example.tprom.R;
 import com.example.tprom.login.Login;
@@ -20,12 +25,21 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileFragment extends Fragment {
     TextView tv_logout;
+    TextView profile_name;
     private GoogleSignInOptions signInOptions;
     private GoogleSignInClient signInClient;
 
+    private SharedPreferences sharedPreferences;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,12 +51,39 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        //return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        profile_name = view.findViewById(R.id.profile_name);
+        // Lấy đối tượng DatabaseReference
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            String userId = currentUser.getUid();
+            DatabaseReference usersRef = FirebaseDatabase.getInstance().getReference().child("users").child(userId);
+// Lắng nghe giá trị dữ liệu một lần
+            usersRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        String username = dataSnapshot.child("username").getValue(String.class);
+                        profile_name.setText(username);
+                        Toast.makeText(getActivity(), username, Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Xử lý khi có lỗi xảy ra
+                    Log.e("FirebaseError", "Error retrieving data: " + databaseError.getMessage());
+                    Toast.makeText(getActivity(), "Error retrieving data", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        return view;
     }
 
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        sharedPreferences = getActivity().getSharedPreferences(AppConstants.PREF_NAME, Context.MODE_PRIVATE);
         signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         signInClient = GoogleSignIn.getClient(getActivity(),signInOptions);
 
@@ -50,6 +91,7 @@ public class ProfileFragment extends Fragment {
         tv_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sharedPreferences.edit().clear().apply();
                 signOut();
             }
         });
