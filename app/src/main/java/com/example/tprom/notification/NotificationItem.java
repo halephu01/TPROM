6ea31f5789bf95ed3c6fb01455de0d3052e20669
Đạt Nguyễn Thành <21521939@ms.uit.edu.vn>
2024-01-12 11:44:11 +0700

    Notification

--- app/src/main/java/com/example/tprom/notification/NotificationItem.java
+++ app/src/main/java/com/example/tprom/notification/NotificationItem.java
@@ -1,19 +1,48 @@
 package com.example.tprom.notification;
 
+import com.example.tprom.properties.Member;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class NotificationItem {
-    static final SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy HH:mm");
-    String Title,GroupName,Description;
-    Date date;
-    public NotificationItem(String title, String groupName, String description, Date date) {
-        Title = title;
-        GroupName = groupName;
-        Description = description;
-        this.date = date;
+    //static final SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy HH:mm");
+    String taskName,groupName,taskDescription;
+    String taskDueTime;
+    int ProgressPercent;
+    ArrayList<Member> assignedUsers;
+    public NotificationItem(){}
+    public NotificationItem(String taskName, String taskDescription){
+        this.taskName=taskName;
+        this.taskDescription=taskDescription;
     }
-    public String getDate(){
-        return simpleDateFormat.format(date);
+    public NotificationItem(String taskName, String groupName, String taskDescription, String taskDueTime) {
+        this.taskName = taskName;
+        this.groupName = groupName;
+        this.taskDescription = taskDescription;
+        this.taskDueTime = taskDueTime;
     }
+    public NotificationItem(String taskName, String groupName, String taskDescription, String taskDueTime, int progressPercent) {
+        this.taskName = taskName;
+        this.groupName = groupName;
+        this.taskDescription = taskDescription;
+        this.taskDueTime = taskDueTime;
+        this.ProgressPercent = progressPercent;
+    }
+    public NotificationItem(String taskName, String groupName, String taskDescription, String taskDueTime, int progressPercent, ArrayList<Member> assignedUsers) {
+        this.taskName = taskName;
+        this.groupName = groupName;
+        this.taskDescription = taskDescription;
+        this.taskDueTime = taskDueTime;
+        this.ProgressPercent = progressPercent;
+        this.assignedUsers=assignedUsers;
+    }
+    public String getTaskDueTime(){
+        return taskDueTime;
+    }
+    public String getTaskName(){ return taskName;}
+    public String getGroupName(){ return groupName;}
+    public String getTaskDescription(){ return taskDescription;}
+    public int getProgressPercent(){ return ProgressPercent;}
+    public ArrayList<Member> getAssignedUsers(){return assignedUsers;}
 }
